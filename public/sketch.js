cf66e9fbb079d4dcc122792f75d0250285f4303b
Anders <anders.erik.lie@gmail.com>
2021-11-15 17:05:39 -0600

    creepy login

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,14 @@
 let socket;
 
+const states = {
+  LOGIN: 'LOGIN',
+  PLAYING: 'PLAYING'
+}
+
+let state = states.LOGIN;
+let username = '';
+
+
 function setup() {
   createCanvas(1200, 600);
   colorMode(HSB, 255);
@@ -15,9 +24,42 @@ function setup() {
   });
 
   // Setup any other event listeners here
+
+  let inp = createInput('');
+  inp.position(200, 200);
+  inp.size(1000);
+  inp.style('font-family', 'Wingdings');
+  inp.style('font-size', '32px');
+  inp.input(() => {
+    //console.log(inp.value());
+    username = inp.value();
+    // Delete extra DOM elements
+    //removeElements();
+  });
+
+  let button = createButton('§¾¿×¬¶þ¤ǢʥʭѬ');
+  button.position(200, 300);
+  button.mousePressed(() => {
+    if (username.length > 0) {
+      socket.emit('login', {username: username});
+      console.log('Login: ' + username);
+      state = states.PLAYING;
+    }
+    removeElements();
+  });
 }
 
 function draw() {
-  background(0);
+  background(220, 255, 255);
+
+  if (state === states.LOGIN) {
+    background(200, 255, 255);
+    //console.log('Asd');
+    textSize(32);
+    text('WELCOME TO CLSLOTHþ UBCLUB SLOTHʭѬ', 50, 50);
+    textSize(24);
+    text('WHAT IS YOUR NAME', 100, 100);
+  }
+
 }
 
